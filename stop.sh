79a2a94493860812d8bd8cb18fae5ec0b48e15a2
Franklin Anyaso <franko172000@gmail.com>
2023-05-11 15:38:59 +0100

    Text correction

--- stop.sh
+++ stop.sh
@@ -4,7 +4,7 @@ set -e
 if [ -x "$(command -v docker)" ]; then
     process_info "Stopping up containers..."
     sudo docker-compose down
-    success "Containers Stopping"
+    success "Containers Stopped"
 
 else
   error "You need to install docker to continue this setup. To install docker, follow this link https://docs.docker.com/engine/install/"
